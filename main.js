49a3a4048cd72c0e91167206c9b563ba97efd73f
touraye <touraye07@gmail.com>
2021-11-23 05:43:13 +0000

    creating todo

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 // include the Node.js 'path' module at the top of your file
 const path = require('path')
 
+let win
+let addWindow
+
 const createWindow = () => {
-	const win = new BrowserWindow({
+	win = new BrowserWindow({
 		width: 800,
 		height: 600,
-		webPreferences: {
-			preload: path.join(__dirname, 'preload.js'),
-		},
 	})
 
-	win.loadFile('index.html')
+	win.loadURL(`file://${__dirname}/main.html`)
+	win.on('closed', () => app.quit())
 }
 
 app.whenReady().then(() => {
@@ -21,8 +22,47 @@ app.whenReady().then(() => {
 	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) createWindow()
 	})
+
+	const mainMenu = Menu.buildFromTemplate(menuTemplate)
+	Menu.setApplicationMenu(mainMenu)
 })
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') app.quit()
 })
+
+let createAddWindow = () => {
+	addWindow = new BrowserWindow({
+		width: 500,
+		height: 300,
+		title: 'Add New Todo',
+	})
+
+	addWindow.loadURL(`file://${__dirname}/todo.html`)
+}
+
+const menuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'New Todo',
+				click() {
+					createAddWindow()
+				},
+			},
+			{
+				label: 'Quit',
+				accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Control+Q',
+				click() {
+					app.quit()
+				},
+			},
+		],
+	},
+]
+
+//add {} on mac
+if (process.platform === 'darwin') {
+	menuTemplate.unshift({})
+}
